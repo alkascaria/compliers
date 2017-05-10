6fb658a59ea0b52bf4e9cb2e75769d5e2913fa18
Daniele Gadler <gadler@rhrk.uni-kl.de>
2017-05-10 21:22:20 +0200

    Invalid Statement file

---
+++ MJInvalidStatement.java
@@ -0,0 +1,67 @@
+package frontend;
+
+import minijava.ast.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Daniele on 10/05/2017.
+ */
+
+//This is the visitor class
+public class MJInvalidStatement extends MJElement.DefaultVisitor
+{
+    MJFrontend frontEndVar;
+
+    public List<SyntaxError> syntaxErrorsFound = new ArrayList<>();
+
+    public void acceptProgram(MJProgram program, MJFrontend frontend)
+    {
+        frontEndVar = frontend;
+        //body of main
+        //TODO: other classes.
+        visit(program.getMainClass().getMainBody());
+
+    }
+
+    @Override public void visit(MJBlock block)
+    {
+        for (MJStatement i : block )
+        {
+           //check if the current i is an expression
+            System.out.println(i.getClass().toString());
+            if(i.getClass().toString().equals("class minijava.ast.MJStmtExprImpl"))
+            {
+                visit((MJStmtExpr)i);
+            }
+            else if(i.getClass().toString().equals("class minijava.ast.class minijava.ast.MJStmtAssignImpl"))
+            {
+                visit((MJStmtAssign)i);
+
+            }
+            else
+            {
+                i.accept(this);
+            }
+        }
+    }
+
+    @Override public void visit(MJStmtExpr stmtExpr)
+    {
+
+        if(stmtExpr.getExpr().getClass().toString().equals("class minijava.ast.MJArrayLookupImpl"))
+        {
+            System.out.println("Bad array found");
+            SyntaxError syntaxError = new SyntaxError(stmtExpr.getExpr(), "Error: Two-Dimensional arrays are not supported in MiniJava.");
+            this.syntaxErrorsFound.add(syntaxError);
+        }
+        else
+        {
+            stmtExpr.getExpr().accept(this);
+        }
+    }
+
+
+
+}
