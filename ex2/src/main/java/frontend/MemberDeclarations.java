24b61267e5586d5d1be12812b1bae2f3471377d6
Praveen Gopal <praveengopal90@gmail.com>
2017-05-11 23:38:21 +0200

    Update

--- ex2/src/main/java/frontend/MemberDeclarations.java
+++ ex2/src/main/java/frontend/MemberDeclarations.java
@@ -15,27 +15,37 @@ public class MemberDeclarations implements MJMemberDecl.MatcherVoid
         varDeclList = MJ.VarDeclList();
         methodDeclList = MJ.MethodDeclList();
     }
-
+    /** adds variable to varDecl List
+     * @param  varDecl(@code MJVarDecl)
+     */
     public void addVarToVarList(MJVarDecl varDecl)
     {
         this.varDeclList.add(0, varDecl);
     }
-
+    /** adds method to method List
+     * @param  methodDecl(@code MJMethodDecl)
+     */
     public void addMethodToMethodList(MJMethodDecl methodDecl)
     {
         this.methodDeclList.add(0, methodDecl);
     }
-
+    /**
+     * Returns variable Declaration List
+     * @return the value {@code varDeclList} returns variable Declaration List
+     */
     public MJVarDeclList getVarDeclList()
     {
         return this.varDeclList;
     }
-
+    /**
+     * Returns method Declaration List
+     * @return the value {@code methodDeclList} returns method Declaration List
+     */
     public MJMethodDeclList getMethodDeclList()
     {
         return this.methodDeclList;
     }
-
+    
     public void addMemberToCorrectList(MJMemberDecl memberDecl)
     {
         memberDecl.match(this);
