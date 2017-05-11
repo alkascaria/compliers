3a243d9986d084a0522d2ac8e5df68e56c252dce
alkascaria <scaria@rhrk.uni-kl.de>
2017-05-11 17:48:28 +0200

    Test cases for stmt-expr and stmt-expr=expr

---
+++ ex2/testdata/parser/error/TestCases-error-st- exp.java
@@ -0,0 +1,32 @@
+/**
+ * Created by alka on 5/11/2017.
+ */
+class TestCaseserror {
+    public static void main(String [] args)
+    {
+        int v;
+	int a;
+
+        v=10;    
+
+        !a;
+
+	-v;
+
+	a[10];
+
+	a.length;
+
+	true;
+
+	false;
+
+	a;
+
+	3;
+
+	this;
+
+	null;
+    }
+}
---
+++ ex2/testdata/parser/error/TestCases-error-st-exp=exp.java
@@ -0,0 +1,23 @@
+/**
+ * Created by alka on 5/11/2017.
+ */
+class TestCaseserror {
+    public static void main(String [] args)
+    {
+        int v;
+        v=10;
+        2*3=4/5;
+
+	b = a + new c();
+
+	if (1<v<100)
+        {
+        	8 = a;    
+        }
+        else
+        {
+        	a+1 = v; 
+        }
+	
+    }
+}
