e3aabdce424cf1a4a9ec13bb9713660b129fca9c
Praveen Gopal <praveengopal90@gmail.com>
2017-05-20 21:06:37 +0200

    + Analysis Java + Todos

--- ex3/src/main/java/analysis/Analysis.java
+++ ex3/src/main/java/analysis/Analysis.java
@@ -18,13 +18,59 @@ public class Analysis {
         this.prog = prog;
     }
 
-
+    /*
+    TODO check program
+    Add errors like
+    "Class already exist"
+    "Class Name should be Unique"
+    "MethodParameter unique"
+    "Extended class should not be main class."
+    "Extended class should be declared."
+    "Field need to be unique"
+    new errors("This program has type errors")
+     */
+    /*
+     */
 
     public void check()
     {
-        this.checkClassNamesDefined();
         //TODO implement type checking here as well!
+        ClassChecker();
+        UniqueMethodParam();
+        UniqueFieldName();
+        UniqueClassName();
+        ExtendedClass();
+        this.checkClassNamesDefined();
+
     }
+
+    public void ClassChecker()
+    {
+        //TODO CHECK CLASS EXIST or NOT
+    }
+
+    public void UniqueMethodParam()
+    {
+        //TODO MethodParameter unique
+    }
+
+    public void UniqueFieldName()
+    {
+        //TODO Unique Field Name
+    }
+
+    public void UniqueClassName()
+    {
+        //Todo Unique Class Name
+    }
+
+    public void ExtendedClass()
+    {
+        //TODO Extended class should not be main class.
+        //TODO Extended class should be declared.
+    }
+
+
 
 
     public void checkClassNamesDefined()
