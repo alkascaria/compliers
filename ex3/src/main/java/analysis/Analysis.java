fb59158a439cb1a3a92d38231fba042569ff0067
DanyEle <gadler@rhrk.uni-kl.de>
2017-05-27 17:19:14 +0200

    + Fixed parameters' subclassing

--- ex3/src/main/java/analysis/Analysis.java
+++ ex3/src/main/java/analysis/Analysis.java
@@ -37,15 +37,19 @@ public class Analysis {
 
 
 
-    public boolean isSubTypeOff(MJType a, MJType b) {
+    public boolean isSubTypeOff(MJType a, MJType b)
+    {
+        //create hashmap with all classes
         HashMap<String, MJClassDecl> table = new HashMap<>();
-        //table.put(prog.getMainClass().getName(), prog.getMainClass());
+
+        //populate hashmap
         for (MJClassDecl classDecl : prog.getClassDecls())
         {
             table.put(classDecl.getName(), classDecl);
         }
 
-        if ((a instanceof MJTypeClass) & (b instanceof MJTypeClass)) {
+        if ((a instanceof MJTypeClass) & (b instanceof MJTypeClass))
+        {
             String classA = ((MJTypeClass) a).getName();
             String classB = ((MJTypeClass) b).getName();
             if (classA.compareTo(classB) == 0)
@@ -53,14 +57,29 @@ public class Analysis {
 
             MJClassDecl clsA = table.get(classA);
 
-            MJExtendsClass tempClass = (MJExtendsClass) clsA.getExtended();
-            while (tempClass != null) {
-                if (tempClass.getName().compareTo(classB) == 0) {
-                    return true;
+            if(clsA.getExtended() instanceof MJExtendsClass)
+            {
+                MJExtendsClass tempClass = (MJExtendsClass) clsA.getExtended();
+
+                while (tempClass != null)
+                {
+                    if (tempClass.getName().compareTo(classB) == 0)
+                    {
+                        return true;
+                    }
+
+                    if(table.get(tempClass.getName()).getExtended() instanceof MJExtendsClass)
+                    {
+                        tempClass = (MJExtendsClass) table.get(tempClass.getName()).getExtended();
+                    }
                 }
-                tempClass = (MJExtendsClass) (table.get(tempClass.getName()).getExtended());
             }
-        } else {
+
+
+
+        }
+        else
+            {
             if (a.toString().compareTo(b.toString()) == 0)
                 return true;
             else
