61538859dd42ea7fe75ae9f29535b6bc7cb8f66c
alkascaria <scaria@rhrk.uni-kl.de>
2017-05-27 23:27:31 +0200

    operators test(uniary binary assignment)

--- ex3/src/main/java/analysis/typechecker.java
+++ ex3/src/main/java/analysis/typechecker.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 
+import static minijava.ast.MJ.TypeClass;
+
 /**
  * Created by alka on 5/27/2017.
  */
@@ -34,19 +36,15 @@ public class typechecker {
             if (!(type instanceof MJTypeInt)) {     //SOP only allows int... so check for that
                 this.errors.add(new TypeError(stmtPrint, "System.out.println can only print an integer"));
             }
-        } else if (stmtPrint.getPrinted() instanceof MJArrayLength)
-        {
+        } else if (stmtPrint.getPrinted() instanceof MJArrayLength) {
 
-        }else if (stmtPrint.getPrinted() instanceof  MJDiv)
-        {
+        } else if (stmtPrint.getPrinted() instanceof MJDiv) {
 
-        }else if (stmtPrint.getPrinted() instanceof  MJUnaryMinus){
+        } else if (stmtPrint.getPrinted() instanceof MJUnaryMinus) {
 
-        }
-        else if (stmtPrint.getPrinted() instanceof  MJExprBinary){
+        } else if (stmtPrint.getPrinted() instanceof MJExprBinary) {
 
-        }
-        else if (!(stmtPrint.getPrinted() instanceof MJNumber)) {
+        } else if (!(stmtPrint.getPrinted() instanceof MJNumber)) {
             //SOP can have a constant number
             this.errors.add(new TypeError(stmtPrint, "System.out.println can only print an integer"));
         }
@@ -71,17 +69,29 @@ public class typechecker {
         if (stmtAssign.getLeft() instanceof MJVarUse) {
             MJVarUse left = (MJVarUse) (stmtAssign.getLeft());
             MJType type = CheckType(left);
-            CheckExpr(type, stmtAssign.getRight());
+            CheckExpr_ofassf(type, stmtAssign.getRight());
         }
     }
 
-    void CheckExpr(MJType type, MJExpr stmtAssign) {
+    void CheckExpr_ofassf(MJType type, MJExpr stmtAssign) {
         if (type instanceof MJType) {
+            System.out.println(type);
+            System.out.println(stmtAssign);
             if (type instanceof MJTypeInt) {
-                if (!(stmtAssign instanceof MJNumber))
+                if (stmtAssign instanceof MJExprBinary) {
+                    if (!(check_exprbinary((MJExprBinary) stmtAssign)))
+                        this.errors.add(new TypeError(stmtAssign, "Variable assignment should be of type int"));
+                } else if (stmtAssign instanceof MJExprUnary) {
+                    check_Expruniary((MJExprUnary) stmtAssign);
+                } else if (!(stmtAssign instanceof MJNumber))
                     this.errors.add(new TypeError(stmtAssign, "Variable assignment should be of type int"));
             } else if (type instanceof MJTypeBool) {
-                if (!(stmtAssign instanceof MJBoolConst))
+                if (stmtAssign instanceof MJExprBinary) {
+                    if (!(check_exprbinary((MJExprBinary) stmtAssign)))
+                        this.errors.add(new TypeError(stmtAssign, "Variable assignment should be of type int"));
+                } else if (stmtAssign instanceof MJExprUnary) {
+                    check_Expruniary((MJExprUnary) stmtAssign);
+                } else if (!(stmtAssign instanceof MJBoolConst))
                     this.errors.add(new TypeError(stmtAssign, "Variable assignment should be of type boolean"));
             } else if (type instanceof MJExprBinary) {
                 if (!(stmtAssign instanceof MJVarUse))
@@ -89,31 +99,57 @@ public class typechecker {
             } else if (type instanceof MJTypeIntArray) {
                 if (!(stmtAssign instanceof MJNewIntArray))
                     this.errors.add(new TypeError(stmtAssign, "Variable assignment should be be Array Int"));
-            }else if(type instanceof MJUnaryMinus) {
-                if (!(stmtAssign instanceof MJVarUse))
-                    this.errors.add(new TypeError(stmtAssign, "Variable assignment should be be Minus"));
-            }else if(type instanceof MJDiv) {
-                if (!(stmtAssign instanceof MJVarUse))
-                    this.errors.add(new TypeError(stmtAssign, "Variable assignment should be be Div"));
             }
         }
     }
 
+    Boolean check_exprbinary(MJExprBinary exprBinary) {
+        System.out.println(exprBinary.getLeft());
+        if ((exprBinary.getLeft() instanceof MJNumber) && ((exprBinary.getRight()) instanceof MJNumber))
+            return true;
+        else if ((exprBinary.getLeft() instanceof MJTypeInt) && ((exprBinary.getRight()) instanceof MJTypeInt))
+            return true;
+        else if ((exprBinary.getLeft() instanceof MJTypeBool) && ((exprBinary.getRight()) instanceof MJTypeBool))
+            return true;
+        else if ((exprBinary.getLeft() instanceof MJVarUse) && ((exprBinary.getRight()) instanceof MJVarUse)) {
+            if (CheckType((MJVarUse) exprBinary.getLeft()).equals(CheckType((MJVarUse) exprBinary.getRight())))
+                return true;
+            else
+                return false;
+        } else
+            return false;
+
+    }
+
+    Boolean check_Expruniary(MJExprUnary exprUnary) {
+        Boolean value = null;
+        if (exprUnary.getUnaryOperator() instanceof MJUnaryMinus) {
+            if (exprUnary.getExpr() instanceof MJNumber) {
+                value = true;
+            } else
+                value = false;
+        } else if (exprUnary.getUnaryOperator() instanceof MJNegate) {
+            if (exprUnary.getExpr() instanceof MJBoolConst)
+                value = true;
+            else
+                value = false;
+        }
+        return value;
+    }
+
     //check if subtyping
-    void CheckReturn(MJStmtReturn stmtReturn, MJMethodDecl methodDecl)
-    {
+
+    void CheckReturn(MJStmtReturn stmtReturn, MJMethodDecl methodDecl) {
         //return type in signature.
         MJType returnType = methodDecl.getReturnType();
         System.out.println(returnType);
 
-        if(stmtReturn.getResult() instanceof MJNewObject)
-        {
+        if (stmtReturn.getResult() instanceof MJNewObject) {
             MJNewObject newObj = (MJNewObject) stmtReturn.getResult();
-            MJType typeObj = new MJTypeClassImpl(newObj.getClassName());
+            MJType typeObj = TypeClass(newObj.getClassName());
             boolean validSubType = StaticMethods.isSubTypeOff(returnType, typeObj);
 
-            if(validSubType == false)
-            {
+            if (validSubType == false) {
                 this.errors.add(new TypeError(newObj, "Return statement is not a subtype of method's return type "));
             }
             //check if returnType doesn't extend newObj
@@ -122,9 +158,6 @@ public class typechecker {
         }
 
     }
-
-
-
 
 
 }
