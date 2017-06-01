80d687d926d7c345e7ad4fecad2416bc320b18a9
DanyEle <gadler@rhrk.uni-kl.de>
2017-06-01 16:01:37 +0200

    + Java and LLVM output comparison checker

--- ex4/src/main/java/frontend/Main.java
+++ ex4/src/main/java/frontend/Main.java
@@ -1,9 +1,15 @@
 package frontend;
 
 import minijava.ast.MJProgram;
+import minillvm.ast.Prog;
+import translation.TestOutputLLVM;
+import translation.Translator;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class Main {
@@ -16,9 +22,17 @@ public class Main {
             fileName = new Scanner(System.in).nextLine();
         }
         try (FileReader r = new FileReader(fileName)) {
+
             MJFrontend frontend = new MJFrontend();
             MJProgram prog = frontend.parse(r);
             System.out.println(prog);
+            Translator translator = new Translator(prog);
+            Prog progTranslated = translator.translate();
+
+            File inputFile = new File(fileName);
+            String input = new String(Files.readAllBytes(inputFile.toPath()), StandardCharsets.UTF_8);
+            TestOutputLLVM.testTranslation(inputFile.getName(), input);
+
 
             frontend.getSyntaxErrors().forEach(System.out::println);
         }
