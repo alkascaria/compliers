79b0ede0c6dcd7686f9380a6cd5bec2f65a1e528
Praveen Gopal <praveengopal90@gmail.com>
2017-06-03 01:16:12 +0200

    + ExprTranslatorMatcher

--- ex4/src/main/java/translation/ExprTranslatorMatcher.java
+++ ex4/src/main/java/translation/ExprTranslatorMatcher.java
@@ -7,84 +7,154 @@ import minijava.ast.*;
 import minillvm.ast.*;
 
 import static minillvm.ast.Ast.*;
+import static minillvm.ast.Ast.VarRef;
 
 /**
  * Created by Daniele on 02/06/2017.
  */
 public class ExprTranslatorMatcher implements MJExpr.Matcher<Operand> {
 
+    /**
+     *
+     * @param fieldAccess
+     * @return
+     */
     @Override
     public Operand case_FieldAccess(MJFieldAccess fieldAccess) {
         return null;
     }
 
+    /**
+     *
+     * @param exprBinary
+     * @return
+     */
     @Override
     public Operand case_ExprBinary(MJExprBinary exprBinary) {
 
         return null;
     }
 
+    /**
+     *
+     * @param exprNull
+     * @return
+     */
     @Override
-    public Operand case_ExprNull(MJExprNull exprNull) {
-        return null;
+    public Operand case_ExprNull(MJExprNull exprNull)
+    {
+
+        return Ast.Nullpointer();
     }
 
+    /**
+     *
+     * @param methodCall
+     * @return
+     */
     @Override
     public Operand case_MethodCall(MJMethodCall methodCall) {
         return null;
     }
 
+    /**
+     *
+     * @param exprUnary
+     * @return
+     */
     @Override
     public Operand case_ExprUnary(MJExprUnary exprUnary) {
         return null;
     }
 
+    /**
+     *
+     * @param boolConst
+     * @return
+     */
     @Override
     public Operand case_BoolConst(MJBoolConst boolConst) {
         return null;
     }
 
+    /**
+     *
+     * @param number
+     * @return
+     */
     @Override
     public Operand case_Number(MJNumber number) {
-        Operand oper = ConstInt(number.getIntValue());
+        //Operand oper = ConstInt(number.getIntValue());
 
-        return oper;
+        //return oper;
+
+        //Optimized
+        return Ast.ConstInt(number.getIntValue());
     }
 
+    /**
+     *
+     * @param varUse(@code MJVarUse)
+     * @return
+     */
     @Override
     public Operand case_VarUse(MJVarUse varUse) {
         //firstly, get the value / reference thatßs in the varuse.
         String varName = varUse.getVarName();
         TemporaryVar tempVar = Translator.varDeclsTempVar.get(varName);
-        TemporaryVar y = TemporaryVar(varName);
-        Load(y,VarRef(tempVar));
-        System.out.println(y);
-
+        TemporaryVar tempVar1 = TemporaryVar(varName);
+        Load(tempVar1,VarRef(tempVar));
+        System.out.println(tempVar1);
 
        //  Operand oper = ConstInt();
-        return null;
+        return Ast.VarRef(tempVar1);
     }
 
+    /**
+     *
+     * @param newIntArray
+     * @return
+     */
     @Override
     public Operand case_NewIntArray(MJNewIntArray newIntArray) {
         return null;
     }
 
+    /**
+     *
+     * @param exprThis
+     * @return
+     */
     @Override
     public Operand case_ExprThis(MJExprThis exprThis) {
         return null;
     }
 
+    /**
+     *
+     * @param arrayLength
+     * @return
+     */
     @Override
     public Operand case_ArrayLength(MJArrayLength arrayLength) {
         return null;
     }
 
+    /**
+     *
+     * @param newObject
+     * @return
+     */
     @Override
     public Operand case_NewObject(MJNewObject newObject) {
         return null;
     }
 
+    /**
+     *
+     * @param arrayLookup
+     * @return
+     */
     @Override
     public Operand case_ArrayLookup(MJArrayLookup arrayLookup) {
         return null;
