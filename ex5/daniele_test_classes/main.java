521f0ab8535189cc9f6ea7ff9349c4e89da38453
DanyEle <gadler@rhrk.uni-kl.de>
2017-07-02 16:55:08 +0200

    ~ Report modified

--- ex5/daniele_test_classes/main.java
+++ ex5/daniele_test_classes/main.java
@@ -1,9 +1,33 @@
-class addition {
+class VarTest {
 	public static void main(String[] args) {
 		
-		System.out.println(new int[-10].length);
-		
+	
+	B b;
+	
+	b = new B();
+	
+	int hallo;
+	
+	
+	System.out.println(b.h(10, 10));
+	
 		
 		
 	}
 }
+
+class B
+{	
+
+	int x;
+	
+	int h(int a, int b)
+	{
+		int c;
+		c = a + b;
+		System.out.println(c);
+		return a;
+	}
+	
+}
+
