d7fb10f157dffbad86555bc805eb137099867fea
alkascaria <scaria@rhrk.uni-kl.de>
2017-07-03 02:45:53 +0200

    Test

--- ex5/daniele_test_classes/Test.java
+++ ex5/daniele_test_classes/Test.java
@@ -3,46 +3,52 @@
  */
 
 class Test {
-	public static void main(String[] args) {
-		A a;
-		B b;
-		a = new A();
-		a.x = 2;
-		System.out.println(a.x);
-		int c;
-		c = a.x;
-		System.out.println(c);
+    public static void main(String[] args) {
+        A a;
+        B b;
+        a = new A();
+        a.x = 2;
+        System.out.println(a.x);
+        int c;
+        c = a.x;
+        System.out.println(c);
 
-	}
+    }
 }
 
 class A {
-	int x;
-	int y;
+    int x;
+    int y;
 
-	int f(int d) {
-		return d;
-	}
+    int f(int d) {
+        return d;
+    }
 
 }
 
 class B extends A {
-	int y;
+    int y;
 
-	int f(int d) {
-		return d;
-	}
+    int f(int d) {
+        return d;
+    }
 }
 
 class C extends B {
-	int x;
+    int x;
 
 
-	int f(int d) {
-		return d;
-	}
+    int f(int d) {
+        if (d == 0) {
+            d = 7;
+        } else {
+            d = 10;
+        }
+        return d;
 
-	int g(int m, int n) {
-		return (m + n);
-	}
+    }
+
+    int g(int m, int n) {
+        return (m + n);
+    }
 }
