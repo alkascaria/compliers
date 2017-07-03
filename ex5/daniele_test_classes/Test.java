32ef10512f8cf094ff6f2013777e8390b41586f2
alkascaria <scaria@rhrk.uni-kl.de>
2017-07-03 02:42:13 +0200

    TestData

---
+++ ex5/daniele_test_classes/Test.java
@@ -0,0 +1,48 @@
+/**
+ * Created by alka on 6/29/2017.
+ */
+
+class Test {
+	public static void main(String[] args) {
+		A a;
+		B b;
+		a = new A();
+		a.x = 2;
+		System.out.println(a.x);
+		int c;
+		c = a.x;
+		System.out.println(c);
+
+	}
+}
+
+class A {
+	int x;
+	int y;
+
+	int f(int d) {
+		return d;
+	}
+
+}
+
+class B extends A {
+	int y;
+
+	int f(int d) {
+		return d;
+	}
+}
+
+class C extends B {
+	int x;
+
+
+	int f(int d) {
+		return d;
+	}
+
+	int g(int m, int n) {
+		return (m + n);
+	}
+}
